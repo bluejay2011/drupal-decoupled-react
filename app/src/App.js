2175434dee1bbcefa4ea7a4aab89c64c15aedd29
Stephanie Jose <stephjose2011@gmail.com>
2020-09-07 20:19:56 +0800

    Establish connection with Drupal article

---
+++ app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import NodeReadWrite from './components/NodeReadWrite';
+
+function App() {
+  return (
+    <NodeReadWrite />
+  );
+}
+
+export default App;
